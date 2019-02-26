6825af1ed1bfd5e1a062664c17aa71b58d313ffc
App Academy Student <bibashkc92@gmail.com>
2019-02-26 17:14:32 -0500

    99Cats W4D2 project

---
+++ ninetyninecats/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create(name: 'Nora', birth_date: '2017/08/08', color: "white", sex: "F", description: "My beautiful daughter I love her so much!!!!")
+Cat.create(name: 'Test', birth_date: '1969/04/20', color: "white", sex: "M", description: "placeholder")
